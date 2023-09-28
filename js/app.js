112ffa4f1ff0e0ce50c51cd12addcc35b1948b53
Augusta Ellison <gustaellison@gmail.com>
2023-09-28 10:48:21 -0400

    connected html to js file

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
 console.log('hello')
+
+// defining an object literal
+//key value pairs <-- important for language
+
+//key is name, value is "Big Foot"
+const bigFoot {
+    name: 'Big Foot',
+    color: 'purple',
+    isReal: true,
+    height: 7, //feet
+    diet: 'vegetarian',
+    woodlandFriends: ['bunny', 'squirrel', 'deer']
+}
+
+
+
