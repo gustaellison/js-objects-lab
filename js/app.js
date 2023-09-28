95daad4b72818634328e52925837e2104284ec03
Augusta Ellison <gustaellison@gmail.com>
2023-09-28 10:40:34 -0400

    oushing

--- js/app.js
+++ js/app.js
@@ -1,9 +1,1 @@
-let sumArray = function(arr) {
-    let total = 0
-    for (let i = 0; i < arr.length - 1; i++){
-        total += arr[i]
-    }
-    return total
-}
-console.log(sumArray([3, 4, 6, 10]))
-console.log(sumArray([5, 7, 9]))
+console.log('hello')
